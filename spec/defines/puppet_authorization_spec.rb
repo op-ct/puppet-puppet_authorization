2e9c228a6787e19fcc3dbdef3da52414b2fad174
Chris Tessmer <chris.tessmer@onyxpoint.com>
2022-08-26 17:41:04 -0400

    Un-hash .with syntax

--- spec/defines/puppet_authorization_spec.rb
+++ spec/defines/puppet_authorization_spec.rb
@@ -8,42 +8,42 @@ describe 'puppet_authorization' do
 
   context 'defaults' do
     it {
-      is_expected.to contain_concat(title).with({
+      is_expected.to contain_concat(title).with(
         path: title,
-        replace: false,
-      })
+        replace: false
+      )
     }
 
     it {
-      is_expected.to contain_concat__fragment("00_header_#{title}").with({
-        target: title,
-      }).with_content(%r{authorization: \{\n  rules: \[\]\n})
+      is_expected.to contain_concat__fragment("00_header_#{title}").with(
+        target: title
+      ).with_content(%r{authorization: \{\n  rules: \[\]\n})
     }
 
     it {
-      is_expected.to contain_concat__fragment("99_footer_#{title}").with({
-        target: title,
-      }).with_content(%r{\}\n})
+      is_expected.to contain_concat__fragment("99_footer_#{title}").with(
+        target: title
+      ).with_content(%r{\}\n})
     }
 
     it {
       is_expected.to contain_hocon_setting("authorization.version.#{title}").
-        that_requires("Concat[#{title}]").with({
+        that_requires("Concat[#{title}]").with(
           path: title,
           setting: 'authorization.version',
-          value: 1,
-        })
+          value: 1
+        )
     }
 
     it {
       is_expected.to contain_hocon_setting(
         "authorization.allow-header-cert-info.#{title}"
       ).
-        that_requires("Concat[#{title}]").with({
+        that_requires("Concat[#{title}]").with(
           path: title,
           setting: 'authorization.allow-header-cert-info',
-          value: false,
-        })
+          value: false
+        )
     }
   end
 
@@ -58,42 +58,42 @@ describe 'puppet_authorization' do
     end
 
     it {
-      is_expected.to contain_concat(title).with({
+      is_expected.to contain_concat(title).with(
         path: '/tmp/foo',
-        replace: true,
-      })
+        replace: true
+      )
     }
 
     it {
-      is_expected.to contain_concat__fragment("00_header_#{title}").with({
-        target: title,
-      }).with_content(%r{authorization: \{\n  rules: \[\]\n})
+      is_expected.to contain_concat__fragment("00_header_#{title}").with(
+        target: title
+      ).with_content(%r{authorization: \{\n  rules: \[\]\n})
     }
 
     it {
-      is_expected.to contain_concat__fragment("99_footer_#{title}").with({
-        target: title,
-      }).with_content(%r{\}\n})
+      is_expected.to contain_concat__fragment("99_footer_#{title}").with(
+        target: title
+      ).with_content(%r{\}\n})
     }
 
     it {
       is_expected.to contain_hocon_setting("authorization.version.#{title}").
-        that_requires("Concat[#{title}]").with({
+        that_requires("Concat[#{title}]").with(
           path: '/tmp/foo',
           setting: 'authorization.version',
-          value: 2,
-        })
+          value: 2
+        )
     }
 
     it {
       is_expected.to contain_hocon_setting(
         "authorization.allow-header-cert-info.#{title}"
       ).
-        that_requires("Concat[#{title}]").with({
+        that_requires("Concat[#{title}]").with(
           path: '/tmp/foo',
           setting: 'authorization.allow-header-cert-info',
-          value: true,
-        })
+          value: true
+        )
     }
   end
 
